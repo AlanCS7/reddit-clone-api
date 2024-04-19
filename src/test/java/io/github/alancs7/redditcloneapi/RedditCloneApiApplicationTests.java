714df80c9b0601c9c3758b7ed055688c8713b36b
Alan Carlos <alanclash2541@gmail.com>
2024-04-18 21:52:58 -0300

    feat: Project created

---
+++ src/test/java/io/github/alancs7/redditcloneapi/RedditCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alancs7.redditcloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
