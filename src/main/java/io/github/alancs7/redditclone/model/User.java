374664e5b5b0563f99390468bc72b07f7bfafb85
Alan Carlos <alanclash2541@gmail.com>
2024-04-18 23:08:13 -0300

    feat: Creating User entity and UserRepository

---
+++ src/main/java/io/github/alancs7/redditclone/model/User.java
@@ -0,0 +1,35 @@
+package io.github.alancs7.redditclone.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Instant createdAt;
+
+    private boolean enabled;
+
+}
