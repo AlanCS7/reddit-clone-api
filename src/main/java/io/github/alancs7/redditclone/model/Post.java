579bd9bcfd9e7b8be16530ca026264b1027e9cff
Alan Carlos <alanclash2541@gmail.com>
2024-04-18 23:07:25 -0300

    feat: Creating Post entity and PostRepository

---
+++ src/main/java/io/github/alancs7/redditclone/model/Post.java
@@ -0,0 +1,39 @@
+package io.github.alancs7.redditclone.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private String name;
+
+    private String url;
+
+    @Lob
+    private String description;
+
+    private Integer voteCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    private Instant createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+
+}
