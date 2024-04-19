e6b2cd843ca7dc3d7e3d5a2789be564e3114222d
Alan Carlos <alanclash2541@gmail.com>
2024-04-18 23:08:49 -0300

    feat: Creating VerificationToken entity and VerificationTokenRepository

---
+++ src/main/java/io/github/alancs7/redditclone/model/VerificationToken.java
@@ -0,0 +1,29 @@
+package io.github.alancs7.redditclone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class VerificationToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private Instant expiryDate;
+
+}
